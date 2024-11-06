d653429d9905d616b8df6fcd6337a3dc31e78abe
Alex Liang <alexliang@arizona.edu>
2024-11-06 16:10:51 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,8 @@
 
 #include <avr/interrupt.h>
 #include <avr/io.h>
-#include "led.h"
 #include "switch.h"
 #include "timer.h"
-#include "lcd.h"
 
 // defines
 
@@ -27,12 +25,15 @@
  * Define a set of states that can be used in the state machine using an enum.
  */
 // typedef enum .......
+
+//slow speed - stepper motor off
+//fast speed - stepper motor on
 typedef enum speedEnum {fast_speed, slow_speed} speedEnum;
 typedef enum stateEnum {wait_press, debounce_press, wait_release, debounce_release} stateType;
 
 
 // Initialize states.  Remember to use volatile 
-volatile speedEnum s = slow_speed;
+volatile speedEnum speed = slow_speed;
 volatile stateType state = wait_press;
 
 
@@ -40,17 +41,11 @@ volatile stateType state = wait_press;
 int main(){
 
   initTimer1();
-  initLCD();
   initSwitchPB3();
-  initLED();
   initTimer0();
-  sei(); // Enable global interrupts.
-  moveCursor(0, 0); // moves the cursor to 0,0 position
-  writeString("Current mode: ");
-  moveCursor(1, 0);  // moves the cursor to 1,0 position
-  writeString("Slow");
 
-// while loop
+  sei(); // Enable global interrupts.
+
   unsigned char led = 0;
   while(1) {
     switch(state){
@@ -67,24 +62,7 @@ int main(){
         state = wait_press;
       break;
     }
-    if (led < 15) {
-      led++;
-    } else if (led == 15) {
-      led = 0;
-    }
-    turnOnLEDWithChar(led);
-    switch(s) {
-      case fast_speed:
-        moveCursor(1, 0);
-        writeString("Fast");
-        delayMs(100);
-      break;
-      case slow_speed:
-      moveCursor(1, 0);
-        writeString("Slow");
-        delayMs(200);
-      break;
-    }
+
   }
   return 0;
 }
@@ -93,14 +71,15 @@ int main(){
 * change at twice the original rate. If the LEDs are already changing at twice
 * the original rate, it goes back to the original rate.
 */
+
 ISR(PCINT0_vect){
   if (state == wait_press){
     state = debounce_press;
   } else if (state == wait_release){
-    if (s == slow_speed){
-      s = fast_speed;
-    } else if (s == fast_speed){
-      s = slow_speed;
+    if (speed == slow_speed){
+      speed = fast_speed;
+    } else if (speed == fast_speed){
+      speed = slow_speed;
     }
     state = debounce_release;
   }
