4a0a45c58b178ee6887149af6420814087415c91
Alex Liang <alexliang@arizona.edu>
2024-11-12 16:06:25 -0700

    stepper motor

--- src/main.cpp
+++ src/main.cpp
@@ -1,109 +1,22 @@
-#include <avr/interrupt.h>
-#include <avr/io.h>
-#include <Arduino.h>
+/* Example sketch to control a stepper motor with L298N motor driver, Arduino UNO and AccelStepper.h library. Contiuous rotation. More info: https://www.makerguides.com */
 
-#include "HX711.h"
+// Include the AccelStepper library:
+#include "AccelStepper.h"
 
+// Define the AccelStepper interface type:
+#define MotorInterfaceType 4
 
-// HX711 circuit wiring
-const int LOADCELL_DOUT_PIN = 2;
-const int LOADCELL_SCK_PIN = 3;
-
-
-HX711 scale;
+// Create a new instance of the AccelStepper class:
+AccelStepper stepper = AccelStepper(MotorInterfaceType, 8, 9, 10, 11);
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("HX711 Demo");
-
-  Serial.println("Initializing the scale");
-
-  // Initialize library with data output pin, clock input pin and gain factor.
-  // Channel selection is made by passing the appropriate gain:
-  // - With a gain factor of 64 or 128, channel A is selected
-  // - With a gain factor of 32, channel B is selected
-  // By omitting the gain factor parameter, the library
-  // default "128" (Channel A) is used here.
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-
-  Serial.println("Before setting up the scale:");
-  Serial.print("read: \t\t");
-  Serial.println(scale.read());			// print a raw reading from the ADC
-
-  Serial.print("read average: \t\t");
-  Serial.println(scale.read_average(20));  	// print the average of 20 readings from the ADC
-
-  Serial.print("get value: \t\t");
-  Serial.println(scale.get_value(5));		// print the average of 5 readings from the ADC minus the tare weight (not set yet)
-
-  Serial.print("get units: \t\t");
-  Serial.println(scale.get_units(5), 1);	// print the average of 5 readings from the ADC minus tare weight (not set) divided
-						// by the SCALE parameter (not set yet)
-
-  scale.set_scale(2280.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
-  scale.tare();				        // reset the scale to 0
-
-  Serial.println("After setting up the scale:");
-
-  Serial.print("read: \t\t");
-  Serial.println(scale.read());                 // print a raw reading from the ADC
-
-  Serial.print("read average: \t\t");
-  Serial.println(scale.read_average(20));       // print the average of 20 readings from the ADC
-
-  Serial.print("get value: \t\t");
-  Serial.println(scale.get_value(5));		// print the average of 5 readings from the ADC minus the tare weight, set with tare()
-
-  Serial.print("get units: \t\t");
-  Serial.println(scale.get_units(5), 1);        // print the average of 5 readings from the ADC minus tare weight, divided
-						// by the SCALE parameter set with set_scale
-
-  Serial.println("Readings:");
+  // Set the maximum speed in steps per second:
+  stepper.setMaxSpeed(1000);
 }
 
 void loop() {
-  Serial.print("one reading:\t");
-  Serial.print(scale.get_units(), 1);
-  Serial.print("\t| average:\t");
-  Serial.println(scale.get_units(10), 1);
-
-  scale.power_down();			        // put the ADC in sleep mode
-  delay(1000);
-  scale.power_up();
+  // Set the speed of the motor in steps per second:
+  stepper.setSpeed(1000);
+  // Step the motor with constant speed as set by setSpeed():
+  stepper.runSpeed();
 }
-
-/*
-#include "loadcell.h"
-#include "timer.h"
-#include "ADC.h"
-
-
-int main(){
-  initADC0();
-  Serial.begin(9600);
-  unsigned int result = 0;
-  float voltage = 0;
-
-  while(1){
-      // print out ADC value
-	// read in ADCL first then read ADCH
-      result = ADCL;
-      result += (ADCH) << 8;
-      //Voltage_k = k * (VHigh/# of levels)
-      voltage = result * (5/1024.0); // 5 v from your kit reads~4.586
-      Serial.println(voltage,2);
-      delayMs(500);
-  }
-
-  return 0;
-}
-
-
-
-
-
-
-ISR(){
-
-}
-*/
