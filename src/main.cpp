fccfde321b086fc632c36c724106eff442ea658c
Elian <cubie23alvarez@gmail.com>
2024-11-14 16:13:34 -0700

    taring

--- src/main.cpp
+++ src/main.cpp
@@ -2,108 +2,49 @@
 #include <avr/io.h>
 #include <Arduino.h>
 
-#include "HX711.h"
+//#include "HX711.h"
 
+
+// HX711 circuit wiring
+// Calibrating the load cell
+#include "HX711.h"
 
 // HX711 circuit wiring
 const int LOADCELL_DOUT_PIN = 2;
 const int LOADCELL_SCK_PIN = 3;
 
-
 HX711 scale;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("HX711 Demo");
-
-  Serial.println("Initializing the scale");
-
-  // Initialize library with data output pin, clock input pin and gain factor.
-  // Channel selection is made by passing the appropriate gain:
-  // - With a gain factor of 64 or 128, channel A is selected
-  // - With a gain factor of 32, channel B is selected
-  // By omitting the gain factor parameter, the library
-  // default "128" (Channel A) is used here.
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-
-  Serial.println("Before setting up the scale:");
-  Serial.print("read: \t\t");
-  Serial.println(scale.read());			// print a raw reading from the ADC
-
-  Serial.print("read average: \t\t");
-  Serial.println(scale.read_average(20));  	// print the average of 20 readings from the ADC
-
-  Serial.print("get value: \t\t");
-  Serial.println(scale.get_value(5));		// print the average of 5 readings from the ADC minus the tare weight (not set yet)
-
-  Serial.print("get units: \t\t");
-  Serial.println(scale.get_units(5), 1);	// print the average of 5 readings from the ADC minus tare weight (not set) divided
-						// by the SCALE parameter (not set yet)
-
-  scale.set_scale(2280.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
-  scale.tare();				        // reset the scale to 0
-
-  Serial.println("After setting up the scale:");
-
-  Serial.print("read: \t\t");
-  Serial.println(scale.read());                 // print a raw reading from the ADC
-
-  Serial.print("read average: \t\t");
-  Serial.println(scale.read_average(20));       // print the average of 20 readings from the ADC
-
-  Serial.print("get value: \t\t");
-  Serial.println(scale.get_value(5));		// print the average of 5 readings from the ADC minus the tare weight, set with tare()
-
-  Serial.print("get units: \t\t");
-  Serial.println(scale.get_units(5), 1);        // print the average of 5 readings from the ADC minus tare weight, divided
-						// by the SCALE parameter set with set_scale
-
-  Serial.println("Readings:");
+  delay(1000);
+    scale.set_scale();    
+    Serial.println("Tare... remove any weights from the scale.");
+    delay(1500);
+    scale.tare();
+    Serial.println("Tare done...");
 }
+
+
 
 void loop() {
-  Serial.print("one reading:\t");
-  Serial.print(scale.get_units(), 1);
-  Serial.print("\t| average:\t");
-  Serial.println(scale.get_units(10), 1);
-
-  scale.power_down();			        // put the ADC in sleep mode
-  delay(1000);
-  scale.power_up();
-}
-
-/*
-#include "loadcell.h"
-#include "timer.h"
-#include "ADC.h"
-
-
-int main(){
-  initADC0();
-  Serial.begin(9600);
-  unsigned int result = 0;
-  float voltage = 0;
-
-  while(1){
-      // print out ADC value
-	// read in ADCL first then read ADCH
-      result = ADCL;
-      result += (ADCH) << 8;
-      //Voltage_k = k * (VHigh/# of levels)
-      voltage = result * (5/1024.0); // 5 v from your kit reads~4.586
-      Serial.println(voltage,2);
-      delayMs(500);
+delay(1000);
+  if (scale.is_ready()) {
+    
+    Serial.print("Place a known weight on the scale...");
+    delay(3000);
+    long reading = scale.get_units(10);
+    reading = reading / -88.10;
+    Serial.print("Result: ");
+    Serial.print(reading);
+    Serial.println("g");
+  } 
+  else {
+    Serial.println("HX711 not found.");
   }
-
-  return 0;
+  
 }
 
-
-
-
-
-
-ISR(){
-
-}
-*/
+//calibration factor will be the (reading)/(known weight)
+//calibration factor found -88.10
