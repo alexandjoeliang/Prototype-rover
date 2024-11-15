afa9e4e186a8f1bae9a9704223e5b256c5399890
Alex Liang <alexliang@arizona.edu>
2024-11-15 01:55:50 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,23 +8,21 @@ void servoSetup() {
   myservo.attach(9);  // attaches the servo on pin 9 to the Servo object
 }
 
-void servoLoop() {
-  for (int pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    myservo.write(pos);  // tell servo to go to position in variable 'pos'
-    delay(15);  // waits 15 ms for the servo to reach the position
-  }
-  for (int pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);  // tell servo to go to position in variable 'pos'
-    delay(15);  // waits 15 ms for the servo to reach the position
-  }
+void servoLoop(int* pos) {
+  myservo.write(*pos);
 }
 
 int main(){
   init();
   servoSetup();
+  int pos = 0;
   while(1){
-    servoLoop();
-
+    servoLoop(&pos);
+    delay(10);
+    pos++;
+    if(pos>270){
+      pos = 0;
+    }
   }
   return 0;
 }
