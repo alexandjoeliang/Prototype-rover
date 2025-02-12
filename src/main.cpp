84ccf00627f45db78f81270ce53cd4e159d71d88
Alex Liang <alexliang@arizona.edu>
2025-02-11 17:36:15 -0700

    motor

--- src/main.cpp
+++ src/main.cpp
@@ -1,90 +1,81 @@
 /*
-   -- New project --
-   
-   This source code of graphical user interface 
-   has been generated automatically by RemoteXY editor.
-   To compile this code using RemoteXY library 3.1.13 or later version 
-   download by link http://remotexy.com/en/library/
-   To connect using RemoteXY mobile app by link http://remotexy.com/en/download/                   
-     - for ANDROID 4.15.01 or later version;
-     - for iOS 1.12.1 or later version;
-    
-   This source code is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.    
+Author  : Andrea Lombardo
+Site    : https://www.lombardoandrea.com
+Source  : https://github.com/AndreaLombardo/L298N/
+
+Here you can see how to work in a common configuration without the needed of Enable pin. 
+
+Make sure your module has a jumper in place.
+
+When L298N has a jumper instead of Enable pin, the speed is always 255.
+
+Wiring schema in file "L298N - Schema_without_EN_pin.png"
 */
 
-//////////////////////////////////////////////
-//        RemoteXY include library          //
-//////////////////////////////////////////////
+// Include the library
+#include <L298N.h>
 
-// you can enable debug logging to Serial at 115200
-//#define REMOTEXY__DEBUGLOG    
+// Pin definition
+const unsigned int IN1 = 2;
+const unsigned int IN2 = 3;
 
-// RemoteXY select connection mode and include library 
-#define REMOTEXY_MODE__ESP8266_HARDSERIAL_POINT
+// Create one motor instance
+L298N motor(IN1, IN2);
 
-
-// RemoteXY connection settings 
-#define REMOTEXY_SERIAL Serial
-#define REMOTEXY_SERIAL_SPEED 115200
-#define REMOTEXY_WIFI_SSID "RemoteXY"
-#define REMOTEXY_WIFI_PASSWORD ""
-#define REMOTEXY_SERVER_PORT 6377
-
-
-#include <RemoteXY.h>
-
-// RemoteXY GUI configuration  
-#pragma pack(push, 1)  
-uint8_t RemoteXY_CONF[] =   // 46 bytes
-  { 255,2,0,0,0,39,0,19,0,0,0,0,31,1,108,200,1,1,2,0,
-  2,22,28,44,22,0,2,26,31,31,79,78,0,79,70,70,0,1,45,88,
-  24,24,0,2,31,0 };
-  
-// this structure defines all the variables and events of your control interface 
-struct {
-
-    // input variables
-  uint8_t switch_01; // =1 if switch ON and =0 if OFF
-  uint8_t button_01; // =1 if button pressed, else =0
-
-    // other variable
-  uint8_t connect_flag;  // =1 if wire connected, else =0
-
-} RemoteXY;   
-#pragma pack(pop)
- 
-/////////////////////////////////////////////
-//           END RemoteXY include          //
-/////////////////////////////////////////////
-
-#define PIN_SWITCH_01 13
-#define PIN_BUTTON_01 12
-
-
-void setup() 
+void setup()
 {
-  RemoteXY_Init (); 
-  
-  pinMode (PIN_SWITCH_01, OUTPUT);
-  pinMode (PIN_BUTTON_01, OUTPUT);
-  
-  // TODO you setup code
-  
+  // Used to display information
+  Serial.begin(9600);
+
+  // Wait for Serial Monitor to be opened
+  while (!Serial)
+  {
+    //do nothing
+  }
 }
 
-void loop() 
-{ 
-  RemoteXY_Handler ();
-  
-  digitalWrite(PIN_SWITCH_01, (RemoteXY.switch_01==0)?LOW:HIGH);
-  digitalWrite(PIN_BUTTON_01, (RemoteXY.button_01==0)?LOW:HIGH);
-  
-  // TODO you loop code
-  // use the RemoteXY structure for data transfer
-  // do not call delay(), use instead RemoteXY_delay() 
+void loop()
+{
 
+  // Tell the motor to go forward (may depend by your wiring)
+  motor.forward();
 
+  // Alternative method:
+  // motor.run(L298N::FORWARD);
+
+  //print the motor status in the serial monitor
+  Serial.print("Is moving = ");
+  Serial.println(motor.isMoving());
+
+  delay(3000);
+
+  // Stop
+  motor.stop();
+
+  // Alternative method:
+  // motor.run(L298N::STOP);
+
+  Serial.print("Is moving = ");
+  Serial.println(motor.isMoving());
+
+  delay(3000);
+
+  // Tell the motor to go back (may depend by your wiring)
+  motor.backward();
+
+  // Alternative method:
+  // motor.run(L298N::BACKWARD);
+
+  Serial.print("Is moving = ");
+  Serial.println(motor.isMoving());
+
+  delay(3000);
+
+  // Stop
+  motor.stop();
+
+  Serial.print("Is moving = ");
+  Serial.println(motor.isMoving());
+
+  delay(3000);
 }
