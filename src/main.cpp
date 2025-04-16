7bde6ba9c7f2d04d2d9f9b81839dae25554b621a
Alex Liang <alexliang@arizona.edu>
2025-04-16 15:31:39 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,41 +2,17 @@
 #include <avr/interrupt.h>
 #include <Arduino.h>
 
-int delayTime = 60UL * 60UL * 1000UL; //1 hour delay
-//int delayTime = 5000; //5 seconds delay (for testing only)
+int speed = 9;
 
-int RelayPin1 = 11; //connect relay module's pin 1 to pin 11 of Arduino
-int RelayPin2 = 12; //connect relay module's pin 2 to pin 12 of Arduino
-
-void forward() {
-  digitalWrite(RelayPin1, LOW); //normally, giving LOW to relay will make it turn off
-  digitalWrite(RelayPin2, HIGH); //and giving HIGH will make it turn on (active low)
-}
-
-void reverse() {
-  digitalWrite(RelayPin2, LOW);
-  digitalWrite(RelayPin1, HIGH);
-}
-
-void stopmotor() {
-  digitalWrite(RelayPin1, LOW);  //so giving HIGH to both relays will make the relays turn off, and stop the motor
-  digitalWrite(RelayPin2, LOW);
-}
-
-void setup() {
-  pinMode(RelayPin1, OUTPUT);
-  pinMode(RelayPin2, OUTPUT);
-  Serial.begin(115200);
+void setup(){
+  
 }
 
 void loop() {
 
-    forward(); //motor turns forward
-    Serial.print("forward");
-      delay(5000);
-
-    stopmotor(); //motor turns reverse
-    Serial.print("backwards");
-      delay(5000);
+  analogWrite(speed, 100); 
+  delay(5000);
+  analogWrite(speed, 0);
+  delay(60000);
   
 }
