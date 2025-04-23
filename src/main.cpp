4fec8e06b9622af35d8ad9482d8705ddd2732869
Alex Liang <alexliang@arizona.edu>
2025-04-22 17:02:37 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,39 @@
 #include <Arduino.h>
 
 int speed = 9;
+int dir = 8;
+bool cw, ccw, pause;
+unsigned long previousMillis = 0;
+const long interval = 2000;
 
 void setup(){
-  
+  pinMode(speed, OUTPUT);
+  pinMode(dir, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
 
-  analogWrite(speed, 100); 
-  delay(5000);
-  analogWrite(speed, 0);
-  delay(60000);
-  
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    
+    previousMillis = currentMillis;
+  }
+
+  if(cw){
+    analogWrite(speed, 150); 
+    digitalWrite(dir, LOW);
+
+  }else if(pause){
+    analogWrite(speed, 0);
+
+  }else if(ccw){
+    analogWrite(speed, 200); 
+    digitalWrite(dir, LOW);
+
+
+  }
+
+
 }
