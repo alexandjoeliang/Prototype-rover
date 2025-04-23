c0166affb58ffb1c281a9d05f0e0685f281ae7ac
Alex Liang <alexliang@arizona.edu>
2025-04-22 17:07:34 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,17 @@ void servoSetup() {
 }
 
 void servoLoop() {
-  for (int pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    myservo.write(pos);  // tell servo to go to position in variable 'pos'
-    delay(15);  // waits 15 ms for the servo to reach the position
+  for (int pos = 114; pos <= 25 + 114; pos += 1) {
+    myservo.write(pos);
+    delay(15); 
   }
-  for (int pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);  // tell servo to go to position in variable 'pos'
-    delay(15);  // waits 15 ms for the servo to reach the position
+  for (int pos = 114 + 25; pos >= 114 - 25; pos -= 1) {
+    myservo.write(pos);
+    delay(15);
+  }
+  for (int pos = 114 - 25; pos <= 114; pos += 1) {
+    myservo.write(pos);
+    delay(15); 
   }
 }
 
